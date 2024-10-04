0b15abc9d3cb5a843c15bc470344cf453f62fb40
Jenny Ibanez cano <jenny.ibanezcano@gmail.com>
2024-10-04 09:35:53 +0100

    lecture read dict and pandas

--- my-work/Lectures/readcsv-dict.py
+++ my-work/Lectures/readcsv-dict.py
@@ -1,4 +1,4 @@
-# simple program to read csv
+# simple program to read csv file as a dictionary
 # Author Jennifer Ibanez
 
 import csv
@@ -12,13 +12,8 @@ DATADIR = "../datafiles/"
         # print(line)
     
 with open (DATADIR + FILENAME, "rt") as fp:
-    reader = csv.reader(fp, delimiter="," , quoting=csv.QUOTE_ALL)
-    linecount = 0
+    reader = csv.DictReader(fp, delimiter="," , quoting=csv.QUOTE_ALL)
     total = 0
     for line in reader:
-        if not linecount:
-            print (f"{line}\n---------------")
-        else:
-            total += int(line[0])
-            print (line)
-        linecount += 1
+        total += int(line['id'])
+        print (line)
