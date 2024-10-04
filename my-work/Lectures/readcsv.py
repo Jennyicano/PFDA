c27d418873abe67bc4251912aa8a31acb8a4ae48
Jenny Ibanez cano <jenny.ibanezcano@gmail.com>
2024-10-04 09:08:55 +0100

    csv lecture

--- my-work/Lectures/readcsv.py
+++ my-work/Lectures/readcsv.py
@@ -6,8 +6,19 @@ import csv
 FILENAME= "data.csv"
 DATADIR = "../datafiles/"
 
+# with open (DATADIR + FILENAME, "rt") as fp:
+    # reader = csv.reader(fp, delimiter="," , quoting=csv.QUOTE_ALL)
+    # for line in reader:
+        # print(line)
+    
 with open (DATADIR + FILENAME, "rt") as fp:
     reader = csv.reader(fp, delimiter="," , quoting=csv.QUOTE_ALL)
+    linecount = 0
+    total = 0
     for line in reader:
-        print(line)
-    
+        if not linecount:
+            print (f"{line}\n---------------")
+        else:
+            total += int(line[0])
+            print (line)
+        linecount += 1
