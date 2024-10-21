3ab7bb7e6d0f1f7b4288c33db49b10ed87ed3518
Jenny Ibanez cano <jenny.ibanezcano@gmail.com>
2024-10-21 22:51:04 +0100

    update lab

---
+++ my-work/Lectures/Lecture04/Lab 04/quiz.py
@@ -0,0 +1,12 @@
+import re
+
+regex = "^Hello [A-Z]"
+filename = "./datafiles/quiz.txt"
+
+with open(filename) as quizFile:
+    for line in quizFile:
+        searchResult = re.search(regex, line)
+        if (searchResult):
+            matchingLine = line
+            # I set the end to blank because each line will already have a \n
+            print (matchingLine, end="")
