9618b0415309458c1c7d5e9453f106a1d01a9c1a
Jenny Ibanez cano <jenny.ibanezcano@gmail.com>
2024-10-21 23:18:24 +0100

    first activity lab 04

--- my-work/Lectures/Lecture04/Lab 04/quiz.py
+++ my-work/Lectures/Lecture04/Lab 04/quiz.py
@@ -1,12 +1,30 @@
+# Quiz program that reads a file and prints out the lines that match the regex
+
 import re
 
-regex = "^Hello [A-Z]"
-filename = "./datafiles/quiz.txt"
+regex = "^$"
+
+filename = "./quiz.txt"
 
 with open(filename) as quizFile:
     for line in quizFile:
         searchResult = re.search(regex, line)
         if (searchResult):
             matchingLine = line
-            # I set the end to blank because each line will already have a \n
+              # I set the end to blank because each line will already have a \n
             print (matchingLine, end="")
+            
+  
+# Results  
+# Input regex: hello = Output: hello
+# Input regex: Hello = Output: Hello, Hello World, Hello mary
+# Input regex: ^Hello = Output: Hello, Hello World
+# Input regex: ^Hell*o = Output: Hello, Hello World, Hello John, Helllllllllllo Anamaniacs
+# Input regex: ^Hell+o = Output: Hello, Hello World, Helllllllllllo Anamaniacs
+# Input regex: ^Hell?o = Output: Hello, Hello World, Hello John
+# Input regex: ^hello [A-Z] = Output: none 
+# Input regex: ^Hello [A-Z] = Output: Hello World 
+# Input regex: "=" = Output: var = 123  
+# Input regex: "#" = Output: change this #this will change
+# Input regex: "[" = Output: error 
+# Input regex: "^$" = Output: none
